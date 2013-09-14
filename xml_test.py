1e17d833a8c48fac62f7d41f25f21d42239fdc11
Richard Neal <rnealx@gmail.com>
2013-09-14 12:09:27 -0400

    More work on parsing XML and such.

--- xml_test.py
+++ xml_test.py
@@ -1,10 +1,12 @@
 from xml.dom import minidom
-xmldoc = minidom.parse('information.xml')
-itemlist = xmldoc.getElementsByTagName('building')
 
-for building in itemlist:
-    print building.attributes['name'].value
-    for card in building.getElementsByTagName('card'):
+def get_xml_data(building_name="The Sem"):
+    xmldoc = minidom.parse(building_name + ".xml")
+    building = xmldoc.getElementsByTagName('building')
+
+    for card in building[0].getElementsByTagName('card'):
     	if card.attributes['type'].value == "facts":
     		for fact in card.getElementsByTagName("fact"):
     			print fact.childNodes[0].nodeValue
+
+get_xml_data()
