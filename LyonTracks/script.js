6bca56f893fec944a572e19f656de1a7ab1edc60
Alicia Herbert <aliciaayn@gmail.com>
2013-10-06 13:26:22 -0400

    website code

---
+++ LyonTracks/script.js
@@ -0,0 +1,24 @@
+function Evan()
+{
+document.getElementById("person_name").innerHTML="Evan Chessman";
+document.getElementById("entry").innerHTML="<p>Evan is a junior</p>";
+
+}
+
+function Alicia()
+{
+document.getElementById("person_name").innerHTML="Alicia Herbert";
+document.getElementById("entry").innerHTML="<p>Alicia is a senior</p>";
+}
+
+function Julia()
+{
+document.getElementById("person_name").innerHTML="Julia Morneau";
+document.getElementById("entry").innerHTML="<p>Julia is a sophomore</p>";
+}
+
+function Richard()
+{
+document.getElementById("person_name").innerHTML="Richard Neal";
+document.getElementById("entry").innerHTML="<p>Richard is a pseudo-senior</p>";
+}
