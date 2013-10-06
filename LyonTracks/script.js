7de70ffde8c701be96d3769510cff40cfa35dccb
Alicia Herbert <aliciaayn@gmail.com>
2013-10-06 17:05:41 -0400

    website changes

--- LyonTracks/script.js
+++ LyonTracks/script.js
@@ -1,24 +1,47 @@
 function Evan()
 {
 document.getElementById("person_name").innerHTML="Evan Chessman";
-document.getElementById("entry").innerHTML="<p>Evan is a junior</p>";
-
+replaceEntryWithFileContents("./people/Evan_Chessman.txt")
 }
 
 function Alicia()
 {
 document.getElementById("person_name").innerHTML="Alicia Herbert";
-document.getElementById("entry").innerHTML="<p>Alicia is a senior</p>";
+replaceEntryWithFileContents("./people/Alicia_Herbert.txt")
 }
 
 function Julia()
 {
 document.getElementById("person_name").innerHTML="Julia Morneau";
-document.getElementById("entry").innerHTML="<p>Julia is a sophomore</p>";
+replaceEntryWithFileContents("./people/Julia_Morneau.txt")
 }
 
 function Richard()
 {
 document.getElementById("person_name").innerHTML="Richard Neal";
-document.getElementById("entry").innerHTML="<p>Richard is a pseudo-senior</p>";
+replaceEntryWithFileContents("./people/Richard_Neal.txt")
+}
+
+
+function replaceEntryWithFileContents(fileName) 
+{
+var xmlhttp;
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("entry").innerHTML=xmlhttp.responseText;
+    }
+  }
+
+xmlhttp.open("GET",fileName,true);
+xmlhttp.send();
 }
