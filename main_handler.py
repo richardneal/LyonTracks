e80fdd428a3cee1096f68aaf6e84b389b1fbf300
Alicia Herbert <aliciaayn@gmail.com>
2013-10-10 11:04:31 -0400

    redirect

--- main_handler.py
+++ main_handler.py
@@ -66,30 +66,32 @@ class MainHandler(webapp2.RequestHandler):
   """Request Handler for the main endpoint."""
 
   def _render_template(self, message=None):
-    """Render the main page template."""
-    template_values = {'userId': self.userid}
-    if message:
-      template_values['message'] = message
-    # self.mirror_service is initialized in util.auth_required.
-    try:
-      template_values['contact'] = self.mirror_service.contacts().get(
-        id='Python Quick Start').execute()
-    except errors.HttpError:
-      logging.info('Unable to find Python Quick Start contact.')
+    #"""Render the main page template."""
+    #template_values = {'userId': self.userid}
+    #if message:
+    #  template_values['message'] = message
+    ## self.mirror_service is initialized in util.auth_required.
+    #try:
+    #  template_values['contact'] = self.mirror_service.contacts().get(
+    #    id='Python Quick Start').execute()
+    #except errors.HttpError:
+    #  logging.info('Unable to find Python Quick Start contact.')
 
-    timeline_items = self.mirror_service.timeline().list(maxResults=3).execute()
-    template_values['timelineItems'] = timeline_items.get('items', [])
+    #timeline_items = self.mirror_service.timeline().list(maxResults=3).execute()
+    #template_values['timelineItems'] = timeline_items.get('items', [])
 
-    subscriptions = self.mirror_service.subscriptions().list().execute()
-    for subscription in subscriptions.get('items', []):
-      collection = subscription.get('collection')
-      if collection == 'timeline':
-        template_values['timelineSubscriptionExists'] = True
-      elif collection == 'locations':
-        template_values['locationSubscriptionExists'] = True
+    #subscriptions = self.mirror_service.subscriptions().list().execute()
+    #for subscription in subscriptions.get('items', []):
+    #  collection = subscription.get('collection')
+    #  if collection == 'timeline':
+    #    template_values['timelineSubscriptionExists'] = True
+    #  elif collection == 'locations':
+    #    template_values['locationSubscriptionExists'] = True
 
-    template = jinja_environment.get_template('LyonTracks/index.html')
-    self.response.out.write(template.render(template_values))
+    #template = jinja_environment.get_template('templates/index.html')
+    #self.response.out.write(template.render(template_values))
+
+	self.redirect('LyonTracks/');
 
   @util.auth_required
   def get(self):
